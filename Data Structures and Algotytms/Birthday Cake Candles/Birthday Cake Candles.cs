e91768eca74ae09769a09bbc395b5cee7a35dfcd
Camyul <vankata.lt@outlook.com>
2018-05-04 14:58:08 +0300

    Solve Birthday cake and Divisible sum

---
+++ Data Structures and Algotytms/Birthday Cake Candles/Birthday Cake Candles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Birthday_Cake_Candles
+{
+    class Program
+    {
+        static int birthdayCakeCandles(int n, int[] ar)
+        {
+            int countResult = 1;
+            int maxLenght = ar[0];
+
+            for (int i = 1; i < ar.Length; i++)
+            {
+                if (maxLenght == ar[i])
+                {
+                    countResult++;
+                }
+                else if (maxLenght < ar[i])
+                {
+                    maxLenght = ar[i];
+                    countResult = 1;
+                }
+            }
+
+            return countResult;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = birthdayCakeCandles(n, ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
